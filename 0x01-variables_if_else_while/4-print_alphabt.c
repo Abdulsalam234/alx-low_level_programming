ba259a9d81a8efaa932138ae2440af3ae74ed0b2
Abdulsalam234 <olanrewajuabdulsalam234@gmail.com>
2022-09-09 10:31:24 -0700

    Task Five

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Print lower and upper case'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet, first, second;
+
+	first = 'e';
+	second = 'q';
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	if (alphabet == first || alphabet == second)
+{
+		continue;
+		putchar(alphabet);
+}
+	putchar('\n');
+	return (0);
+}
