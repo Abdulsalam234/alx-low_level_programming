b896a967e337ef1daea08aa90934d990fe7ec5f5
Abdulsalam234 <olanrewajuabdulsalam234@gmail.com>
2022-09-09 10:51:12 -0700

    Task Six

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'Print lower and upper case'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+		printf("%d", j);
+	printf("\n");
+
+
+	return (0);
+}
