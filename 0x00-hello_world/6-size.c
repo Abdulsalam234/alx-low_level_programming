c7e62dbb33a6fff70e6b93e4b6999d8b47691ed1
Abdulsalam234 <olanrewajuabdulsalam234@gmail.com>
2022-09-11 15:20:32 -0700

    Task six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,5 +19,6 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
 	printf("Size of a float: %zu byte(s)\n", sizeof(e));
+	printf("\n");
 	return (0);
 }
