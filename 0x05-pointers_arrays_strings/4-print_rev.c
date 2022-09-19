f2492fd3ba9338bf7a484f6eb6d10fb539f3e817
Abdulsalam234 <olanrewajuabdulsalam234@gmail.com>
2022-09-19 10:15:28 -0700

    Task five

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in  reverse order
+ * @s: string to be reversed
+ */
+void print_rev(char *s)
+{
+	int len = 0, i;
+
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		putchar(s[i]);
+
+	putchar('\n');
+}
