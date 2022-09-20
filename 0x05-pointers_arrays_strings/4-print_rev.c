71a62962c28e14c1cce3166ae233a241d9c8d039
Abdulsalam234 <olanrewajuabdulsalam234@gmail.com>
2022-09-20 01:09:52 -0700

    Task five

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,13 +5,17 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int fcounter = 0;
+	int i, n;
 
-	while (s[i++])
-		len++;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		fcounter++;
+	}
 
-	for (i = len - 1; i >= 0; i--)
-		putchar(s[i]);
-
+	for (n = (fcounter - 1); n >= 0; n--)
+	{
+		putchar(s[n]);
+	}
 	putchar('\n');
 }
