f2417c5608f1286c206a8527b1e3ec6e3f2a2722
Abdulsalam234 <olanrewajuabdulsalam234@gmail.com>
2022-09-22 09:50:42 -0700

    8. rot13 encoding

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,1 +1,29 @@
 #include "main.h"
+/**
+ * rot13 - Function that encodes using rot13
+ * @s: Value of string to be encoded
+ *
+ * Return: Prints the encoded string
+ *
+ **/
+char *rot13(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+		{
+			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
+			{
+				s[a] -= 13;
+				break;
+			}
+
+			s[a] += 13;
+			break;
+		}
+		a++;
+	}
+	return (s);
+}
