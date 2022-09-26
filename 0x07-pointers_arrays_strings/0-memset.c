637d0b1a4084e375a9bd238cb0d841ccba482070
Abdulsalam234 <olanrewajuabdulsalam234@gmail.com>
2022-09-26 03:56:13 -0700

    Task One

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+void *_memset(void *s, int b, size_t n)
+{
+	unsigned int i;
+	unsigned char *mem;
+	unsigned char value;
+	s = *mem;
+	c = value;
+
+	for (i = 0; i < n; i++)
+	{
+		mem[i] = value;
+	}
+	return (mem);
+}
